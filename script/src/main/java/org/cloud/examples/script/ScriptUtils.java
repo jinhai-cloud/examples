c0e7213acba472c3e6511b0787e2e5119af92fee
jinhai-cloud <jinhai.cloud@gmail.com>
2023-11-08 18:41:19 +0800

    add ScriptUtils

---
+++ script/src/main/java/org/cloud/examples/script/ScriptUtils.java
@@ -0,0 +1,26 @@
+package org.cloud.examples.script;
+
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.Map;
+
+public final class ScriptUtils {
+    private static final ScriptEngine GROOVY = new ScriptEngineManager().getEngineByName("groovy");
+
+    private ScriptUtils() {
+    }
+
+    public static Object eval(String script, Map<String, Object> map, boolean isOnce) throws ScriptException {
+        Bindings bindings = GROOVY.createBindings();
+        if (map != null && !map.isEmpty()) {
+            bindings.putAll(map);
+        }
+
+        if (isOnce) {
+            bindings.put("#jsr223.groovy.engine.keep.globals", "phantom");
+        }
+        return GROOVY.eval(script, bindings);
+    }
+}
