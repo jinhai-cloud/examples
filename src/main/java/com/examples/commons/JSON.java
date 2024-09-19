03175dc1c3967f8e405cbd2433bc3980fd241614
jinhai-cloud <jinhai.cloud@gmail.com>
2024-09-19 20:01:32 +0800

    add stream chat

--- src/main/java/com/examples/commons/JSON.java
+++ src/main/java/com/examples/commons/JSON.java
@@ -2,9 +2,6 @@ package com.examples.commons;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -28,9 +25,6 @@ public final class JSON {
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern(PATTERN);
 
     private static final JsonMapper MAPPER = JsonMapper.builder()
-            .enable(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS)
-            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-            .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
             .addModule(new Jdk8Module())
             .addModule(new JavaTimeModule()
                     .addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DATE_FORMAT))
--- src/main/java/com/examples/concurrent/SentinelExample.java
+++
@@ -1,8 +0,0 @@
-package com.examples.concurrent;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-public class SentinelExample {
-
-}
