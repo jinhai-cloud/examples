459a52d725880c40e28080220763d78ac3064685
jinhai-cloud <jinhai.cloud@gmail.com>
2024-10-07 00:08:47 +0800

    refactor SpEL

--- src/main/java/com/examples/script/SpEL.java
+++ src/main/java/com/examples/script/SpEL.java
@@ -2,24 +2,19 @@ package com.examples.script;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import com.google.common.hash.Hashing;
 import org.apache.commons.collections4.MapUtils;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.SpelCompilerMode;
-import org.springframework.expression.spel.SpelParserConfiguration;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Objects;
 
 public class SpEL {
-    private static final ExpressionParser PARSER = new SpelExpressionParser(
-            new SpelParserConfiguration(SpelCompilerMode.IMMEDIATE, SpEL.class.getClassLoader()));
-    private static final Cache<String, Expression> CACHE = Caffeine.newBuilder().build();
+    private static final ExpressionParser PARSER = new SpelExpressionParser();
+    private static final Cache<ScriptKey, Expression> CACHE = Caffeine.newBuilder().build();
 
     public static <V> Object eval(String script, Map<String, V> map) {
         Objects.requireNonNull(script);
@@ -29,8 +24,8 @@ public class SpEL {
             map.forEach(context::setVariable);
         }
 
-        String cacheKey = Hashing.murmur3_128().hashString(script, StandardCharsets.UTF_8).toString();
-        Expression expression = CACHE.get(cacheKey, key -> PARSER.parseExpression(script));
+        ScriptKey scriptKey = new ScriptKey(script);
+        Expression expression = CACHE.get(scriptKey, key -> PARSER.parseExpression(script));
         return expression.getValue(context);
     }
 }
