4388fa004acb193c7ef2d68b08d85a00a981cf93
jinhai-cloud <jinhai.cloud@gmail.com>
2024-10-08 11:37:41 +0800

    add mustache

--- src/main/java/com/examples/commons/Placeholder.java
+++ src/main/java/com/examples/commons/Placeholder.java
@@ -1,15 +1,21 @@
 package com.examples.commons;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
 import org.apache.commons.text.StringSubstitutor;
 import org.slf4j.helpers.MessageFormatter;
 import org.springframework.util.PropertyPlaceholderHelper;
 
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Map;
 
 public final class Placeholder {
 
     private static final PropertyPlaceholderHelper helper =
             new PropertyPlaceholderHelper("${", "}");
+    private static final MustacheFactory MUSTACHE = new DefaultMustacheFactory();
 
     private Placeholder() {
     }
@@ -26,5 +32,10 @@ public final class Placeholder {
 
     public static String resolve(String template, Map<String, String> map) {
         return helper.replacePlaceholders(template, map::get);
+    }
+
+    public static <V> String apply(String template, Map<String, V> data) {
+        Mustache mustache = MUSTACHE.compile(new StringReader(template), "template");
+        return mustache.execute(new StringWriter(), data).toString();
     }
 }
