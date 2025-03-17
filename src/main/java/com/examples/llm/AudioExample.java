1b926e39a76a6572fe134d6c66c5d5eca5bdc242
jinhai-cloud <jinhai.cloud@gmail.com>
2025-03-17 14:38:23 +0800

    add asr&tts

--- src/main/java/com/examples/llm/AudioExample.java
+++ src/main/java/com/examples/llm/AudioExample.java
@@ -1,10 +1,7 @@
 package com.examples.llm;
 
 import java.nio.ByteBuffer;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.*;
 
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
-import com.alibaba.cloud.ai.dashscope.audio.DashScopeSpeechSynthesisModel;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
 import com.alibaba.dashscope.audio.ttsv2.SpeechSynthesisAudioFormat;
@@ -31,25 +27,32 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/audio")
 public class AudioExample {
     private final DashScopeChatModel chatModel;
-    private final DashScopeSpeechSynthesisModel speechSynthesisModel;
+    private final TargetDataLine asr;
     private final SourceDataLine tts;
 
     @Value("${spring.ai.dashscope.api-key}")
     private String API_KEY;
 
+    private static final String ASR_MODEL = "paraformer-realtime-v2";
     private static final String TTS_MODEL = "cosyvoice-v2";
     private static final String VOICE = "longxiaochun_v2";
 
-    public AudioExample(DashScopeChatModel chatModel, DashScopeSpeechSynthesisModel speechSynthesisModel)
-            throws LineUnavailableException {
+    public AudioExample(DashScopeChatModel chatModel) throws LineUnavailableException {
         this.chatModel = chatModel;
-        this.speechSynthesisModel = speechSynthesisModel;
 
-        AudioFormat format = new AudioFormat(48000, 16, 1, true, false);
-        tts = AudioSystem.getSourceDataLine(format);
-        tts.open(format);
+        AudioFormat asrFormat = new AudioFormat(16000, 16, 1, true, false);
+        asr = AudioSystem.getTargetDataLine(asrFormat);
+        asr.open(asrFormat);
+        asr.start();
+
+        AudioFormat ttsFormat = new AudioFormat(48000, 16, 1, true, false);
+        tts = AudioSystem.getSourceDataLine(ttsFormat);
+        tts.open(ttsFormat);
         tts.start();
     }
+
+    @GetMapping("/asr/stream")
+    public void asrStream() {}
 
     @GetMapping("/tts/call")
     public void ttsCall(@RequestParam(defaultValue = "给我讲一个笑话") String message) throws NoApiKeyException {
