bf2602253a60f608b05f15032849c2c23d81c407
jinhai-cloud <jinhai.cloud@gmail.com>
2025-11-05 15:09:03 +0800

    update pom

--- src/main/java/com/examples/commons/BeanMapper.java
+++
@@ -1,9 +0,0 @@
-package com.examples.commons;
-
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
-
-@Mapper
-public interface BeanMapper {
-    BeanMapper INSTANCE = Mappers.getMapper(BeanMapper.class);
-}
